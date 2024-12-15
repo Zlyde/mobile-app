d2287b8186d39e8454050e48eae16ceea8dc533a
Teophil Simao <teophilsimao@gmail.com>
2024-12-15 21:31:03 +0100

    textchange

--- src/utils/ApiCall.jsx
+++ src/utils/ApiCall.jsx
@@ -2,7 +2,7 @@ export const fetchCities = async () => {
   try {
     const response = await fetch('http://localhost:5001/api/city')
     if (!response.ok) {
-      throw new Error('could not fetch')
+      throw new Error('could not fetch cities')
     }
     return await response.json()
   } catch (error) {
