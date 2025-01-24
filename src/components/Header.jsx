0ab5aa5e8aad695b288455b2813ebc9a31d9494e
Anton Lindahl <lindahl.anton@outlook.com>
2025-01-24 13:53:29 +0100

    logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
+import ZoomiesLogo from "../assets/Zoomies.png";
+
 const Header = () => {
   const title = "ZOOMIES";
 
   return (
     <header className="header">
+      <img src={ZoomiesLogo} alt="Zoomies Logo" className="header-logo" />
       <h1>{title}</h1>
     </header>
   );
